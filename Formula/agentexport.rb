5b33d6eb8b990467b6d0de9bce432ec076a6a031
Nico Ritschel <ritschel@gmail.com>
2026-01-04 12:56:53 -0800

    Update agentexport to v0.2.0

--- Formula/agentexport.rb
+++ Formula/agentexport.rb
@@ -1,26 +1,26 @@
 class Agentexport < Formula
   desc "Share Claude Code and Codex session transcripts with end-to-end encryption"
   homepage "https://github.com/nicosuave/agentexport"
-  version "0.1.13"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nicosuave/agentexport/releases/download/v#{version}/agentexport-#{version}-macos-arm64.tar.gz"
-      sha256 "4bbe4d8bafe274afc03216580b936d49edaf21811c389f96f7515ff8a5b21c22"
+      sha256 "5c814b667384fcb098d2ea270de5abda35de83bca5a46fd613177bf9bd02d2ec"
     else
       url "https://github.com/nicosuave/agentexport/releases/download/v#{version}/agentexport-#{version}-macos-x86_64.tar.gz"
-      sha256 "8ce4dc09cc8029654d2312137de92a267868628383959b12be298ce2dbeaa804"
+      sha256 "b3cac716439e2e5b7e39570a455945909a34e8f9654439b10fa368486873eb1f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/nicosuave/agentexport/releases/download/v#{version}/agentexport-#{version}-linux-arm64.tar.gz"
-      sha256 "3aa45e62ae764182912a8c12ba0601e723b4677746d92b4a7f7ae312377857e4"
+      sha256 "932ba8699d87ed775e806b68c1a2b59dda5dd7d00ba939d76d08416125f847d2"
     else
       url "https://github.com/nicosuave/agentexport/releases/download/v#{version}/agentexport-#{version}-linux-x86_64.tar.gz"
-      sha256 "40615ddef2d4c2fb7710b76354da81617e7249b9b926bd72979fc0b90993ac5a"
+      sha256 "84e444207014baf00bcf8677cdf1c185ee2b06e25ad70b3332dc74f1a8812a59"
     end
   end
 
