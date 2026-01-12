dd8b461647215a13e646c6887eb6a9face596ec3
Nico Ritschel <ritschel@gmail.com>
2026-01-11 21:03:40 -0800

    Roll back memex to v0.2.1 (v0.2.2/v0.2.3 had broken tags)

--- Formula/memex.rb
+++ Formula/memex.rb
@@ -1,26 +1,26 @@
 class Memex < Formula
   desc "Fast local history search for Claude and Codex logs"
   homepage "https://github.com/nicosuave/memex"
-  version "0.2.3"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-#{version}-macos-arm64.tar.gz"
-      sha256 "555f48494a9f4263484581ad469e028aee0ed41e0f32eeb2027a6b3abb8f7daa"
+      sha256 "1ccdd0f44eda1c0f468b0ef9a27c2f013de0cbfd11131b8a9dd71822ae2e90ba"
     else
       url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-#{version}-macos-x86_64.tar.gz"
-      sha256 "e435c36827ac2409a2f7ee46fca74c8e82d5b91ba9a228eccc8863b0945e400b"
+      sha256 "ece8360f7b2d100b3a06a826d21619e94529775827a125c95f0f19f38dfb913b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-#{version}-linux-arm64.tar.gz"
-      sha256 "711d588b5cb8230333baa4921c4f757e1ce6df5a07aa6dea4efff93ac7af7b7a"
+      sha256 "fcb84f992fb47350094075c2af1db6401ebc575131884e9960b20944f6a58cb6"
     else
       url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-#{version}-linux-x86_64.tar.gz"
-      sha256 "19c3d39dba1ca5721ff7c4ba552106a68a32b0cf7a34298cb89ba46772cc97fe"
+      sha256 "8a56a00590b673b389e0c44d2c0b86b561ebd48156bbde25784c25a1ec335c3f"
     end
   end
 
