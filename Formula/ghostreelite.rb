b98495319b4157c8eea181191ce97cb0823cea86
Nico Ritschel <ritschel@gmail.com>
2026-03-18 17:34:04 -0700

    Add gmx and ghostreelite formulas
    Placeholder SHA256s, will be updated on first release.

---
+++ Formula/ghostreelite.rb
@@ -0,0 +1,24 @@
+class Ghostreelite < Formula
+  desc "Worktree manager with zmx sessions + Ghostty splits"
+  homepage "https://github.com/nicosuave/gmx"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/nicosuave/gmx/releases/download/v#{version}/ghostreelite-#{version}-macos-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/nicosuave/gmx/releases/download/v#{version}/ghostreelite-#{version}-macos-x86_64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "ghostreelite"
+  end
+
+  test do
+    assert_match "ghostreelite", shell_output("#{bin}/ghostreelite --help")
+  end
+end
