f42f0a567523d14fb4ecbc40ca1934f766ae28db
Nico Ritschel <ritschel@gmail.com>
2026-08-11 19:52:32 -0700

    Fix comradex 0.9.2 formula

--- Formula/comradex.rb
+++ Formula/comradex.rb
@@ -5,20 +5,20 @@ class Comradex < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/nicosuave/comradex/releases/download/v0.9.1/comradex-0.9.1-macos-arm64.tar.gz"
+      url "https://github.com/nicosuave/comradex/releases/download/v0.9.2/comradex-0.9.2-macos-arm64.tar.gz"
       sha256 "f12f7f7d94f4067cf15f41e05589fe2901fc9b392129b43a885bdb8d019b323b"
     else
-      url "https://github.com/nicosuave/comradex/releases/download/v0.9.1/comradex-0.9.1-macos-x86_64.tar.gz"
+      url "https://github.com/nicosuave/comradex/releases/download/v0.9.2/comradex-0.9.2-macos-x86_64.tar.gz"
       sha256 "63d9025cb8ed793bd78a8196f585cfefc74e82bb7178970456028b706c2e5681"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/nicosuave/comradex/releases/download/v0.9.1/comradex-0.9.1-linux-arm64.tar.gz"
+      url "https://github.com/nicosuave/comradex/releases/download/v0.9.2/comradex-0.9.2-linux-arm64.tar.gz"
       sha256 "cde74bcd3756101f1320746ccbf0a4f8ae58c37d6cb5b17b4e9855c0d837fd4d"
     else
-      url "https://github.com/nicosuave/comradex/releases/download/v0.9.1/comradex-0.9.1-linux-x86_64.tar.gz"
+      url "https://github.com/nicosuave/comradex/releases/download/v0.9.2/comradex-0.9.2-linux-x86_64.tar.gz"
       sha256 "be33e6bd26f0bebc10e83abaaff4f726d0d5367e95f29298aee33112c72e9723"
     end
   end
