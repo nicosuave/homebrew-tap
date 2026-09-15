c334be5d5092558e029730f09e358775875e1713
Nico Ritschel <ritschel@gmail.com>
2026-09-14 18:37:26 -0700

    Update Memex app to 0.20.0

--- Casks/memex-app.rb
+++ Casks/memex-app.rb
@@ -1,12 +1,13 @@
 cask "memex-app" do
-  version "0.19.7"
-  sha256 "90d7b128eee47eba8b6c681976c53dad2751b315b50480ca41a8c9a69e20016f"
+  version "0.20.0"
+  sha256 "dab965fe2207fe7d010e86ab751dc65a032b94b06d9a2ba5f44a66ca568a91e6"
 
-  url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-app-#{version}-macos-universal.zip"
+  url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-app-#{version}-macos-arm64.zip"
   name "Memex"
   desc "Browse and search local agent conversation history"
   homepage "https://github.com/nicosuave/memex"
 
+  depends_on arch: :arm64
   depends_on macos: :sonoma
 
   app "Memex.app"
