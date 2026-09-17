5b385002a336cf5af9126f09f84784ecd976a197
Nico Ritschel <ritschel@gmail.com>
2026-09-17 14:10:46 -0700

    Update Memex app to 0.22.0

--- Casks/memex-app.rb
+++ Casks/memex-app.rb
@@ -1,6 +1,6 @@
 cask "memex-app" do
-  version "0.20.0"
-  sha256 "dab965fe2207fe7d010e86ab751dc65a032b94b06d9a2ba5f44a66ca568a91e6"
+  version "0.22.0"
+  sha256 "1d8f1e55a09fcc12a6206c10c49e4ed79f086bfcdbc580c509dc5a27464a42ec"
 
   url "https://github.com/nicosuave/memex/releases/download/v#{version}/memex-app-#{version}-macos-arm64.zip"
   name "Memex"
